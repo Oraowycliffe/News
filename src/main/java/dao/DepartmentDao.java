3167fbc83c00c334ded64ea9183d4b0bd97c9cf1
Oraowycliffe <oraowycliffe@gmail.com>
2019-07-10 01:01:26 +0300

    Initialize the DataAccess Objects for the Department class

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Department;
+
+import java.util.List;
+
 public interface DepartmentDao {
+
+     void add(Department department);
+
+
+     List<Department> getAll();
+     Department getDepartmentById(int id);
+
+
+     void update(int id, String name, String description, int employees);
+
+     void deleteById(int id);
+
+
+     void clearAll();
+
 }

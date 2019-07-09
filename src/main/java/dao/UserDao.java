9cf554d1a9c63d429d7d15f983dcfe3f11cabd4a
Oraowycliffe <oraowycliffe@gmail.com>
2019-07-10 01:02:30 +0300

    Initialize the DataAccess Objects for the User class

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,23 @@
 package dao;
 
-public interface User {
+import models.User;
+
+import java.util.List;
+
+public interface UserDao {
+    //create new user
+
+    void add(User user);
+
+
+    //retrieve user
+    List<User>getAll();
+    List<User>getAllUsersInADepartment(int departmentid);
+
+    //update
+    void update (int id, String name, String position, String role, int departmentId);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }

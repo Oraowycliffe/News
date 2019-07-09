ae092055e9bf9b41e577e7ca0ed78d7bac2e1be1
Oraowycliffe <oraowycliffe@gmail.com>
2019-07-10 01:02:03 +0300

    Initialize the DataAccess Objects for the News class

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+
+    //retrieve
+    List<News> getAll();
+    List<News> getAllNewsForDepartment(int departmentId);
+
+    //Update
+    void update(int id, String content,int departmentId);
+
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
 }
